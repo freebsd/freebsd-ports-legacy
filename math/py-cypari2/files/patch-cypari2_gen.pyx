freebsd/freebsd-ports-legacy

commit c557a0f6c1557fb1a3100df5ce69ce604ed6512c

math/pari: Update 2.11.4 -> 2.13.0
Bump 15 depending ports.

Reported by:	portscout


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@553237 35697150-7ecd-e111-bb59-0022644237b5

math/py-cypari2/Makefile
@@ -3,6 +3,7 @@
 
 PORTNAME=	cypari2
 DISTVERSION=	2.1.1
+PORTREVISION=	1
 CATEGORIES=	math python
 PKGNAMEPREFIX=	${PYTHON_PKGNAMEPREFIX}
 

devel/py-cysignals/Makefile
@@ -3,6 +3,7 @@
 
 PORTNAME=	cysignals
 DISTVERSION=	1.10.2
+PORTREVISION=	1
 CATEGORIES=	devel python
 PKGNAMEPREFIX=	${PYTHON_PKGNAMEPREFIX}
 

lang/clisp/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	clisp
 PORTVERSION=	2.49.93+
-PORTREVISION=	1
+PORTREVISION=	2
 CATEGORIES=	lang lisp
 
 MAINTAINER=	kiri@TrueFC.org

math/eclib/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	eclib
 PORTVERSION=	20190909
-PORTREVISION=	1
+PORTREVISION=	2
 DISTVERSIONPREFIX=	v
 CATEGORIES=	math
 

math/giacxcas/Makefile
@@ -3,6 +3,7 @@
 
 PORTNAME=	giacxcas
 DISTVERSION=	1.6.0-7
+PORTREVISION=	1
 CATEGORIES=	math
 MASTER_SITES=	http://www-fourier.ujf-grenoble.fr/~parisse/debian/dists/stable/main/source/:giac \
 		http://cocoa.dima.unige.it/cocoalib/tgz/:cocoa \

math/gp2c/Makefile
@@ -3,6 +3,7 @@
 
 PORTNAME=	gp2c
 DISTVERSION=	0.0.11pl4
+PORTREVISION=	1
 CATEGORIES=	math
 MASTER_SITES=	http://pari.math.u-bordeaux.fr/pub/pari/GP2C/
 

math/lcalc/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	lcalc
 PORTVERSION=	1.23
-PORTREVISION=	1
+PORTREVISION=	2
 CATEGORIES=	math
 MASTER_SITES=	ftp://ftp.fu-berlin.de/unix/misc/sage/spkg/upstream/lcalc/
 

math/pari/Makefile
@@ -2,8 +2,7 @@
 # $FreeBSD$
 
 PORTNAME=	pari
-DISTVERSION=	2.11.4
-PORTREVISION=	1
+DISTVERSION=	2.13.0
 PORTEPOCH=	2
 CATEGORIES=	math
 MASTER_SITES=	http://pari.math.u-bordeaux.fr/pub/pari/unix/ \

commit 1698db48a392bece720059f457a142bcba21245f

Fix a breakage introduced by r553237: Use polisirreducible() instead of isirreducible()
Obtained from:	upstream (7bf4211)


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@553498 35697150-7ecd-e111-bb59-0022644237b5

math/py-cypari2/Makefile
@@ -3,7 +3,7 @@
 
 PORTNAME=	cypari2
 DISTVERSION=	2.1.1
-PORTREVISION=	1
+PORTREVISION=	2
 CATEGORIES=	math python
 PKGNAMEPREFIX=	${PYTHON_PKGNAMEPREFIX}
 

math/py-cypari2/files/patch-cypari2_gen.pyx
@@ -1,0 +1,11 @@
+--- cypari2/gen.pyx.orig	2019-04-23 10:57:51 UTC
++++ cypari2/gen.pyx
+@@ -4109,7 +4109,7 @@ cdef class Gen(Gen_base):
+         non-constant polynomial, or False if f is reducible or constant.
+         """
+         sig_on()
+-        t = isirreducible(self.g)
++        t = polisirreducible(self.g)
+         clear_stack()
+         return t != 0
+ 

math/py-cypari2/files/patch-cypari2_paridecl.pxd
@@ -1,0 +1,11 @@
+--- cypari2/paridecl.pxd.orig	2019-04-23 10:57:51 UTC
++++ cypari2/paridecl.pxd
+@@ -3850,7 +3850,7 @@ cdef extern from *:     # PARI headers already include
+     GEN     glcm0(GEN x, GEN y)
+     GEN     gp_factor0(GEN x, GEN flag)
+     GEN     idealfactorback(GEN nf, GEN L, GEN e, int red)
+-    long    isirreducible(GEN x)
++    long    polisirreducible "isirreducible"(GEN x)
+     GEN     newtonpoly(GEN x, GEN p)
+     GEN     nffactorback(GEN nf, GEN L, GEN e)
+     GEN     nfrootsQ(GEN x)