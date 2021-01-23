freebsd/freebsd-ports-legacy

commit 6d3270cce9f4e5ffbdf7a6abb6f6700255dc464d

math/flint2: Update 2.6.3 -> 2.7.0
Reported by:	portscout


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@558890 35697150-7ecd-e111-bb59-0022644237b5

math/flint2/Makefile
@@ -2,7 +2,7 @@
 
 PORTNAME=	flint2
 DISTVERSIONPREFIX=	v
-DISTVERSION=	2.6.3
+DISTVERSION=	2.7.0
 CATEGORIES=	math
 
 MAINTAINER=	yuri@FreeBSD.org
@@ -12,7 +12,8 @@ LICENSE=	GPLv2
 LICENSE_FILE=	${WRKSRC}/LICENSE
 
 LIB_DEPENDS=	libgmp.so:math/gmp \
-		libmpfr.so:math/mpfr
+		libmpfr.so:math/mpfr \
+		libopenblas.so:math/openblas
 
 USES=		cmake compiler:c++11-lang
 USE_GITHUB=	yes

math/flint2/distinfo
@@ -1,3 +1,3 @@
-TIMESTAMP = 1597336021
-SHA256 (wbhart-flint2-v2.6.3_GH0.tar.gz) = 26f00856ba35c8c44fc2642972f4f0248ef7157e1f59a49a8cebce31bc01ff52
-SIZE (wbhart-flint2-v2.6.3_GH0.tar.gz) = 4526459
+TIMESTAMP = 1608615265
+SHA256 (wbhart-flint2-v2.7.0_GH0.tar.gz) = c1885e64faf0eccc0d48e0a4346ae7c862e858dcb6809890232cbba0b917d2e3
+SIZE (wbhart-flint2-v2.7.0_GH0.tar.gz) = 4943442

math/flint2/pkg-plist
@@ -21,6 +21,7 @@ include/flint/fmpq.h
 include/flint/fmpq_mat.h
 include/flint/fmpq_matxx.h
 include/flint/fmpq_mpoly.h
+include/flint/fmpq_mpoly_factor.h
 include/flint/fmpq_poly.h
 include/flint/fmpq_polyxx.h
 include/flint/fmpq_vec.h
@@ -40,6 +41,7 @@ include/flint/fmpz_mod_poly_factor.h
 include/flint/fmpz_mod_poly_factorxx.h
 include/flint/fmpz_mod_polyxx.h
 include/flint/fmpz_mpoly.h
+include/flint/fmpz_mpoly_factor.h
 include/flint/fmpz_poly.h
 include/flint/fmpz_poly_factor.h
 include/flint/fmpz_poly_factorxx.h
@@ -61,6 +63,7 @@ include/flint/fq_nmod.h
 include/flint/fq_nmod_embed.h
 include/flint/fq_nmod_mat.h
 include/flint/fq_nmod_mpoly.h
+include/flint/fq_nmod_mpoly_factor.h
 include/flint/fq_nmod_poly.h
 include/flint/fq_nmod_poly_factor.h
 include/flint/fq_nmod_vec.h
@@ -74,6 +77,8 @@ include/flint/fq_vec_templates.h
 include/flint/fq_zech.h
 include/flint/fq_zech_embed.h
 include/flint/fq_zech_mat.h
+include/flint/fq_zech_mpoly.h
+include/flint/fq_zech_mpoly_factor.h
 include/flint/fq_zech_poly.h
 include/flint/fq_zech_poly_factor.h
 include/flint/fq_zech_vec.h
@@ -92,9 +97,11 @@ include/flint/mpfr_mat.h
 include/flint/mpfr_vec.h
 include/flint/mpn_extras.h
 include/flint/mpoly.h
+include/flint/n_poly.h
 include/flint/nmod_mat.h
 include/flint/nmod_matxx.h
 include/flint/nmod_mpoly.h
+include/flint/nmod_mpoly_factor.h
 include/flint/nmod_poly.h
 include/flint/nmod_poly_factor.h
 include/flint/nmod_poly_mat.h
@@ -125,5 +132,5 @@ include/flint/tuple.h
 include/flint/ulong_extras.h
 include/flint/vector.h
 lib/libflint.so
-lib/libflint.so.14
-lib/libflint.so.14.0.3
+lib/libflint.so.15
+lib/libflint.so.15.0.0

commit 3ac138d3e2c40a54cac93012bd944315a420e319

math/e-antic has been broken since r558890 of math/flint2: upgrade it to 1.0.0-rc.12 in order to fix it.
Release notes at <https://github.com/flatsurf/e-antic/releases>.


git-svn-id: svn+ssh://svn.freebsd.org/ports/head@562411 35697150-7ecd-e111-bb59-0022644237b5

math/e-antic/Makefile
@@ -2,7 +2,7 @@
 # $FreeBSD$
 
 PORTNAME=	e-antic
-PORTVERSION=	0.1.8
+DISTVERSION=	1.0.0-rc.12
 #MASTER_SITES=	https://www.labri.fr/perso/vdelecro/e-antic/
 CATEGORIES=	math
 
@@ -12,25 +12,55 @@ COMMENT=	Real Embedded Algebraic Number Theory In C
 LICENSE=	GPLv3 LGPL3
 LICENSE_COMB=	dual
 
-LIB_DEPENDS=	libarb.so:math/arb	\
-		libgmp.so:math/gmp	\
-		libflint.so:math/flint2
+BUILD_DEPENDS=	${PYTHON_PKGNAMEPREFIX}setuptools>0:devel/py-setuptools@${PY_FLAVOR}
+LIB_DEPENDS=	libarb.so:math/arb		\
+		libgmp.so:math/gmp		\
+		libantic.so:math/antic		\
+		libflint.so:math/flint2		\
+		libbenchmark.so:devel/benchmark	\
+		libboost_thread.so:devel/boost-libs
+
+USES=		autoreconf compiler:c++11-lang gmake libtool localbase	\
+		python:3.7+
 
 USE_GITHUB=	yes
-GH_ACCOUNT=	videlec
+ANTIC_TAG=	f4e8a60
+CATCH_TAG=	ff349a5
+CEREAL_TAG=	51cbda5
+FLINT_TAG=	ae7ec89
+UF_TAG=		0000f42
+GH_ACCOUNT=	flatsurf catchorg:c1 USCiLab:c2 wbhart:f flatsurf:u
+GH_PROJECT=	${PORTNAME} antic:a Catch2:c1 cereal:c2 flint2:f	\
+		unique-factory:u
+GH_TAGNAME=	${ANTIC_TAG}:a ${CATCH_TAG}:c1 ${CEREAL_TAG}:c2 ${FLINT_TAG}:f	\
+		${UF_TAG}:u
 
-USES=		autoreconf compiler:c++11-lang gmake libtool localbase
-
-OPTIONS_DEFINE=	DOCS VALGRIND
+CONFIGURE_ARGS=	--without-pytest # pytest requires cppyy
 
 LIBS+=		"-lm"
 GNU_CONFIGURE=	yes
 USE_LDCONFIG=	yes
 TEST_TARGET=	check
 
-VALGRIND_DESC=			Enable Valgrind on the unit tests
-VALGRIND_BUILD_DEPENDS=		valgrind:devel/valgrind
-VALGRIND_TEST_DEPENDS=		valgrind:devel/valgrind
-VALGRIND_CONFIGURE_ENABLE=	valgrind
+PLIST_SUB=	VER=1.0.0rc12
+
+post-extract:
+	${CP} -R ${WRKDIR}/antic-${ANTIC_TAG}/* ${WRKSRC}/libeantic/upstream/antic/
+	${CP} -R ${WRKDIR}/Catch2-${CATCH_TAG}/*	\
+		${WRKSRC}/libeantic/test/external/catch2/
+	${CP} -R ${WRKDIR}/cereal-${CEREAL_TAG}/*	\
+		${WRKSRC}/libeantic/test/external/cereal/
+	${CP} -R ${WRKDIR}/flint2-${FLINT_TAG}/* ${WRKSRC}/libeantic/upstream/antic/
+	${CP} -R ${WRKDIR}/unique-factory-${UF_TAG}/*	\
+		${WRKSRC}/libeantic/srcxx/external/unique-factory
+
+post-install:
+	${REINPLACE_CMD} -e 's|${STAGEDIR}||'	\
+		${STAGEDIR}${PREFIX}/lib/python3.7/site-packages/pyeantic/install_files.txt
+	${RM} ${STAGEDIR}${PREFIX}/lib/python3.7/site-packages/pyeantic/install_files.txt.bak
+	${PYTHON_CMD} ${PYTHON_LIBDIR}/compileall.py -d \
+		${PYTHONPREFIX_SITELIBDIR} -f ${STAGEDIR}${PYTHONPREFIX_SITELIBDIR}
+	${PYTHON_CMD} -O ${PYTHON_LIBDIR}/compileall.py -d \
+		${PYTHONPREFIX_SITELIBDIR} -f ${STAGEDIR}${PYTHONPREFIX_SITELIBDIR}
 
 .include <bsd.port.mk>

math/e-antic/distinfo
@@ -1,3 +1,13 @@
-TIMESTAMP = 1596476102
-SHA256 (videlec-e-antic-0.1.8_GH0.tar.gz) = 7c7c7693f86d8bb6d849d903cdb6796491d845a9fd79e8a3c9166edbf2436bc0
-SIZE (videlec-e-antic-0.1.8_GH0.tar.gz) = 127960
+TIMESTAMP = 1611416419
+SHA256 (flatsurf-e-antic-1.0.0-rc.12_GH0.tar.gz) = df42a10a2381cebf401d920258db6ad2599a0e64bd4866c8b6df41128df48e28
+SIZE (flatsurf-e-antic-1.0.0-rc.12_GH0.tar.gz) = 122879
+SHA256 (catchorg-Catch2-ff349a5_GH0.tar.gz) = 45983da4c511bc6ed68e391178ead52b0b40c5e8ad7ea5f3fc8c376432d1b6b7
+SIZE (catchorg-Catch2-ff349a5_GH0.tar.gz) = 655437
+SHA256 (USCiLab-cereal-51cbda5_GH0.tar.gz) = efa214cfc850a4a2360baf4419b88c93be2a08b09ff5f9bac604a18498b83cfb
+SIZE (USCiLab-cereal-51cbda5_GH0.tar.gz) = 335754
+SHA256 (wbhart-flint2-ae7ec89_GH0.tar.gz) = b31afe615382f2174b19a951d89602787a0314d8ae0602be8a2987c7861d5aca
+SIZE (wbhart-flint2-ae7ec89_GH0.tar.gz) = 4526427
+SHA256 (flatsurf-unique-factory-0000f42_GH0.tar.gz) = 69c9ee22e36ff871709403f4715f9acff3b9cfa5e6b715ec1e21543bb8f32419
+SIZE (flatsurf-unique-factory-0000f42_GH0.tar.gz) = 8247
+SHA256 (flatsurf-antic-f4e8a60_GH0.tar.gz) = e3dc2b4caf4c80f83c2e391ff6b8bce2c4b3694028a4fe7b07696411be0a07e4
+SIZE (flatsurf-antic-f4e8a60_GH0.tar.gz) = 73895

math/e-antic/files/patch-libeantic_src_fmpz__poly__extra_randtest__irreducible.c
@@ -1,0 +1,40 @@
+--- libeantic/src/fmpz_poly_extra/randtest_irreducible.c.orig	2021-01-21 20:23:35 UTC
++++ libeantic/src/fmpz_poly_extra/randtest_irreducible.c
+@@ -18,14 +18,25 @@ void fmpz_poly_randtest_irreducible(fmpz_poly_t p, fli
+     slong i;
+     fmpz_t c;
+     fmpz_mod_poly_t q;
++#if __FLINT_RELEASE >= 20700
++    fmpz_mod_ctx_t ctx;
++#endif
+ 
+     fmpz_init(c);
+ 
+     fmpz_randprime(c, state, bits, 0);
++#if __FLINT_RELEASE >= 20700
++    fmpz_mod_ctx_init(ctx, c);
++    fmpz_mod_poly_init(q, ctx);
++    fmpz_mod_poly_randtest_irreducible(q, state, len, ctx);
++
++    fmpz_mod_poly_get_fmpz_poly(p, q, ctx);
++#else
+     fmpz_mod_poly_init(q, c);
+     fmpz_mod_poly_randtest_irreducible(q, state, len);
+ 
+     fmpz_mod_poly_get_fmpz_poly(p, q);
++#endif
+ 
+     /* After lifting, the coefficients belong to {0, ..., c-1}. We now  */
+     /* randomly subtract c so that some of them become negative.        */
+@@ -38,6 +49,11 @@ void fmpz_poly_randtest_irreducible(fmpz_poly_t p, fli
+                 c);
+     }
+ 
++#if __FLINT_RELEASE >= 20700
++    fmpz_mod_poly_clear(q, ctx);
++    fmpz_mod_ctx_clear(ctx);
++#else
+     fmpz_mod_poly_clear(q);
++#endif
+     fmpz_clear(c);
+ }

math/e-antic/pkg-plist
@@ -1,17 +1,39 @@
+include/e-antic/cppyy.h
 include/e-antic/e-antic.h
-include/e-antic/nf.h
-include/e-antic/nf_elem.h
-include/e-antic/poly_extra.h
+include/e-antic/fmpq_poly_extra.h
+include/e-antic/fmpz_poly_extra.h
 include/e-antic/renf.h
 include/e-antic/renf_elem.h
 include/e-antic/renfxx.h
+include/e-antic/renfxx_cereal.h
+include/e-antic/renfxx_fwd.h
+include/libeantic/e-antic.h
 lib/libeantic.a
 lib/libeantic.so
-lib/libeantic.so.0
-lib/libeantic.so.0.0.0
+lib/libeantic.so.1
+lib/libeantic.so.1.0.0
 lib/libeanticxx.a
 lib/libeanticxx.so
-lib/libeanticxx.so.0
-lib/libeanticxx.so.0.0.0
-%%PORTDOCS%%%%DOCSDIR%%/README
-%%PORTDOCS%%%%DOCSDIR%%/poly_extra.txt
+lib/libeanticxx.so.1
+lib/libeanticxx.so.1.0.0
+%%PYTHON_SITELIBDIR%%/pyeantic-1.0.0rc12-py%%PYTHON_VER%%.egg-info/PKG-INFO
+%%PYTHON_SITELIBDIR%%/pyeantic-1.0.0rc12-py%%PYTHON_VER%%.egg-info/SOURCES.txt
+%%PYTHON_SITELIBDIR%%/pyeantic-1.0.0rc12-py%%PYTHON_VER%%.egg-info/dependency_links.txt
+%%PYTHON_SITELIBDIR%%/pyeantic-1.0.0rc12-py%%PYTHON_VER%%.egg-info/requires.txt
+%%PYTHON_SITELIBDIR%%/pyeantic-1.0.0rc12-py%%PYTHON_VER%%.egg-info/top_level.txt
+%%PYTHON_SITELIBDIR%%/pyeantic/__init__.py
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/__init__.cpython-%%PYTHON_SUFFIX%%.opt-1.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/__init__.cpython-%%PYTHON_SUFFIX%%.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/cppyy_eantic.cpython-%%PYTHON_SUFFIX%%.opt-1.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/cppyy_eantic.cpython-%%PYTHON_SUFFIX%%.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/real_embedded_number_field.cpython-%%PYTHON_SUFFIX%%.opt-1.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/real_embedded_number_field.cpython-%%PYTHON_SUFFIX%%.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/realalg_conversion.cpython-%%PYTHON_SUFFIX%%.opt-1.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/realalg_conversion.cpython-%%PYTHON_SUFFIX%%.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/sage_conversion.cpython-%%PYTHON_SUFFIX%%.opt-1.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/__pycache__/sage_conversion.cpython-%%PYTHON_SUFFIX%%.pyc
+%%PYTHON_SITELIBDIR%%/pyeantic/cppyy_eantic.py
+%%PYTHON_SITELIBDIR%%/pyeantic/install_files.txt
+%%PYTHON_SITELIBDIR%%/pyeantic/real_embedded_number_field.py
+%%PYTHON_SITELIBDIR%%/pyeantic/realalg_conversion.py
+%%PYTHON_SITELIBDIR%%/pyeantic/sage_conversion.py

math/flint2/Makefile
@@ -2,8 +2,10 @@
 
 PORTNAME=	flint2
 DISTVERSIONPREFIX=	v
-DISTVERSION=	2.7.1
+DISTVERSION=	2.7.1	# See note #1
 CATEGORIES=	math
+# Note #1: FLINT is known to change its API. When upgrading please check that
+#	it does not break its consumers (e.g. math/e-antic)
 
 MAINTAINER=	yuri@FreeBSD.org
 COMMENT=	Fast library for number theory